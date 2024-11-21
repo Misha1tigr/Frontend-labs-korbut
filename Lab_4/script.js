5efc79f3930d0aadc5360dafe171a4a81557038e
Misha <misha1tigr@gmail.com>
2024-11-21 13:32:29 +0200

    Added code for task 2 (image contols)

--- Lab_4/script.js
+++ Lab_4/script.js
@@ -7,3 +7,41 @@ document.querySelector("#hobbies li:nth-child(2)").addEventListener("click", fun
     this.classList.toggle("active-2");
 });
 
+// Завдання 2
+const imageControls = document.getElementById("imageControls");
+const cityImage = document.getElementById("cityImage");
+
+const addedImages = [];
+
+imageControls.addEventListener("click", (event) => {
+    if (event.target.id === "addImage") {
+        // Клонувати зображення
+        const newImage = cityImage.cloneNode();
+        newImage.style.width = cityImage.clientWidth + "px"; // Копіювати розмір оригіналу на нове зображення
+        cityImage.parentNode.insertBefore(newImage, imageControls); // Вставляємо нове фото перед кнопками
+        addedImages.push(newImage); // Додаємо до списку доданих зображень
+    } else if (event.target.id === "increaseImage") {
+        if (addedImages.length > 0) {
+            // Збільшуємо останню копію
+            const latestImage = addedImages[addedImages.length - 1];
+            latestImage.style.width = latestImage.clientWidth + 20 + "px";
+        } else {
+            // Збільшуємо оригінал
+            cityImage.style.width = cityImage.clientWidth + 20 + "px";
+        }
+    } else if (event.target.id === "decreaseImage") {
+        if (addedImages.length > 0) {
+            // Зменшуємо останню копію
+            const latestImage = addedImages[addedImages.length - 1];
+            latestImage.style.width = latestImage.clientWidth - 20 + "px";
+        } else {
+            // Зменшуємо оригінал
+            cityImage.style.width = cityImage.clientWidth - 20 + "px";
+        }
+    } else if (event.target.id === "removeImage") {
+        if (addedImages.length > 0) {
+            const latestImage = addedImages.pop(); // Видаляємо зображення з списку доданих зображень
+            latestImage.remove(); // Видаляємо зображення з документа
+        }
+    }
+});
